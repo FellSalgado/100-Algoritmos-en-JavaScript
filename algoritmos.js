98bc94ef595935738eee7d5a8a1f1bc10fa64782
Felix <salgadocanga@gmail.com>
2023-03-01 00:18:16 -0500

    agregado los algoritmos #51, #52, ..., hasta el #90

--- algoritmos.js
+++ algoritmos.js
@@ -639,3 +639,566 @@ if (algoritmo == "#50 comprando dolares") {
 
     alert(`con $${cartera} usted puede comprar ${dolares} USD`);
 }
+
+if (algoritmo == "#51 descuentos del dia de la mujer") {
+    /* Un almacen abre un promocion exclusiva en el dia de la mujer, donde dara descuentos a todos,
+	pero especialmente a las mujeres,
+	Desarrolle un programa que lea el nombre, el genero y el valor de las compras del cliente y calcule el precio
+	con descuento, teniendo en cuenta que:
+	-Los homres ganan un 5% de descuento
+	-Las mujeres ganan un 13% de descuento */
+
+    let nombre = prompt("Ingrese el nombre");
+    let genero = prompt("Ingrese el genero h/m");
+    let valorCompra = parseInt(prompt("ingrese el valor de la compra"));
+    let descuentoHombres = (5 * valorCompra) / 100;
+    let descuentoMujeres = (13 * valorCompra) / 100;
+
+    if (genero == "h") {
+        alert(
+            `la compra del sr.${nombre}\n dio ${
+                valorCompra - descuentoHombres
+            } con el 5% de descuento`
+        );
+    } else if (genero == "m") {
+        alert(
+            `la compra de la sra.${nombre}\n dio ${
+                valorCompra - descuentoMujeres
+            } con el 13% de descuento`
+        );
+    } else {
+        alert("ingrese una genero valido h/m");
+    }
+}
+
+if (algoritmo == "#52 formar un triangulo") {
+    /* desarrolle un programa que lea el tamaño de tres
+	segmentos de una recta. Analice sus medidas y diga si es posible formar
+	un triangulo con esas rectas, Matematicamente, para que tres segmentos formen un triangulo, cada medida de cada
+	lado debe ser menor a la suma de los otros dos. */
+
+    let recta1 = 5;
+    let recta2 = 5;
+    let recta3 = 5;
+    if (
+        recta1 < recta2 + recta3 &&
+        recta2 < recta1 + recta3 &&
+        recta3 < recta1 + recta2
+    ) {
+        alert(`Es posible formar un triangulo`);
+    } else {
+        alert(`No es posible formar un triangulo`);
+    }
+}
+
+if (algoritmo == "#53 el area de un terreno") {
+    /* cree un programa que lea el ancho y el alto de un terreno
+	rectangular, calculando y mostrando su area en m². El programa tambien deve mostrar la clasificacion del terreno,
+	deacuerdo con la lista de abajo:
+	-abajo de 100m2 = terreno regular
+	-entre 100m2 y 500m2 = terreno master
+	-arriba de los 500m2 = terreno VIP */
+
+    let largo = 26;
+    let ancho = 20;
+    let area = largo * ancho;
+
+    if (area < 100) {
+        alert(
+            `El area total del terreno es ${area}m², su terreno es un terreno regular`
+        );
+    } else if (area > 100 && area <= 500) {
+        alert(`El area total del terreno es ${area}m², su terreno es master`);
+    } else {
+        alert(`El area total del terreno es ${area}m², su terreno es VIP`);
+    }
+}
+
+if (algoritmo == "#54 confirmar la contraseña") {
+    let password = prompt("Ingrese la nueva contraseña");
+    let newPassword = prompt("Confirma la nueva contraseña");
+
+    alert(
+        password == newPassword
+            ? "Genial, se ha cambiado tu contraseña"
+            : "las contraseñas no coinciden"
+    );
+}
+
+if (algoritmo == "#55 tipo de triangulo") {
+    /* desarrolle un programa que lea el tamaño de tres
+	segmentos de una recta. Analice sus medidas y diga si es posible formar
+	un triangulo con esas rectas, Matematicamente, para que tres segmentos formen un triangulo, cada medida de cada
+	lado debe ser menor a la suma de los otros dos.
+
+	ademas, si los segmentos pueden formar un triangulo, el programa debera mostrar si el triangulo es de tipo:
+	- EQUILATERO: Todos los lados iguales.
+	- ISOSCELES: Dos lados iguales.
+	- ESCALENO: Todos los lados diferentes. */
+
+    let recta1 = 5;
+    let recta2 = 6;
+    let recta3 = 9;
+    if (
+        recta1 < recta2 + recta3 &&
+        recta2 < recta1 + recta3 &&
+        recta3 < recta1 + recta2
+    ) {
+        console.log(`✔ Es posible formar un triangulo`);
+        if (recta1 == recta2 && recta2 == recta3) {
+            console.log("de tipo EQUILATERO");
+        } else if (recta1 == recta2 || recta1 == recta3 || recta2 == recta3) {
+            console.log("de tipo ISOSCELES");
+        } else if (recta1 != recta2 && recta2 != recta3) {
+            console.log("de tipo ESCALENO");
+        }
+    } else {
+        console.log(`No es posible formar un triangulo`);
+    }
+}
+
+if (algoritmo == "#56 del 6 al 11") {
+    /* escriba un programa que muestre lo siguiente en pantalla:
+	6 7 8 9 10 11 termino */
+
+    let i = 6;
+    while (i < 12) {
+        console.log(i++);
+    }
+    console.log("termino");
+}
+
+if (algoritmo == "#57 cuenta regresiva") {
+    /* escriba un programa que muestre lo siguiente en pantalla:
+	5 4 3 2 1 despegue 🚀 */
+
+    let i = 5;
+    while (i > 0) {
+        console.log(i--);
+    }
+    console.log("despegue 🚀");
+}
+
+if (algoritmo == "#58 de tres en tres") {
+    /* escriba un programa que muestre lo siguiente en pantalla:
+	0 3 6 9 12 15 18 termino*/
+
+    let i = 0;
+    while (i < 18) {
+        console.log((i += 3));
+    }
+    console.log("Termino");
+}
+
+if (algoritmo == "#59 valor inicial y valor final") {
+    /* Desarrolle un programa que le permita al usuario ingresar un numero entero
+	positivo inicial y un numero final, para luego mostarle todos los numeros entre estos dos
+	ej:
+	ingrese el primer numero: 2
+	ingrese el segundo numero: 5
+	-> salida: 2, 3, 4, 5 */
+
+    let primerNumero = prompt("Ingrese el primer numero");
+    let segundoNumero = prompt("Ingrese el segundo numero");
+
+    while (primerNumero <= segundoNumero) {
+        console.log(primerNumero++);
+    }
+}
+
+if (algoritmo == "#60 divididos por 4") {
+    /* desarrolle un algoritmo que muestre una cuenta regresiva del 30 al 1,
+	marcando los numero que al dividirse por cuatro no tomen decimales. */
+    let numero = 30;
+
+    while (numero > 0) {
+        if (numero % 4 == 0) {
+            console.log([numero--]);
+        } else {
+            console.log(numero--);
+        }
+    }
+}
+
+if (algoritmo == "#61 valor inicial, valor final y incremento") {
+    /* desarrolle un algoritmo que lea un valor inicial del contador,
+	un valor final y un incremento, mostrando enseguida todos los valores del intervalo.
+	ej:
+	Digite el primer valor: 10
+	Digite el ultimo valor: 20
+	Digite el incremento: 2 */
+
+    let inicial = 2;
+    let final = 20;
+    let incremento = 2;
+
+    if (inicial < final) {
+        while (inicial <= final) {
+            console.log(inicial);
+            inicial += incremento;
+        }
+    } else {
+        while (inicial >= final) {
+            console.log(inicial);
+            inicial -= incremento;
+        }
+    }
+}
+
+if (algoritmo == "#62 fizzBuzz") {
+    function fizzBuzz() {
+        for (let i = 0; i <= 50; i++) {
+            if (i % 15 == 0) console.log("fizzbuzz");
+            else if (i % 3 == 0) console.log("fiz");
+            else if (i % 5 == 0) console.log("buzz");
+            else console.log(i);
+        }
+    }
+}
+
+if (algoritmo == "#63 de cinco en cinco") {
+    /* desarrolle un algoritmo utilizando la estructura "para" que muestre
+	lo siguiente en la pantalla: 0 5 10 15 20 25 30 termino! */
+
+    for (let i = 0; i <= 30; i += 5) {
+        console.log(i);
+    }
+    console.log("termino!");
+}
+
+if (algoritmo == "#64 numeros primos") {
+    const primerNumero = (number) => {
+        if (number < 2) {
+            return false;
+        }
+
+        for (let i = 2; i < number; i++) {
+            if (number % i === 0) return false;
+        }
+
+        return true;
+    };
+
+    for (let i = 0; i <= 100; i++) {
+        primerNumero(i) && console.log(i);
+    }
+}
+
+if (algoritmo == "#65 saludar al usuario") {
+    const saludar = (name) => alert("Hola " + name);
+
+    saludar(prompt("ingrese su nombre"));
+}
+
+if (algoritmo == "#66 área de un polígono") {
+    function calcularArea(polygon, x, y) {
+        if (polygon === "Triángulo") {
+            return (x * y) / 2;
+        }
+        return x * y;
+    }
+
+    console.log(`Triangulo = ${calcularArea("Triángulo", 10, 10)}`);
+    console.log(`Cuadrado o rectangulo = ${calcularArea("", 10, 10)}`);
+}
+
+if (algoritmo == "#67 Convertir Booleano a String") {
+    const booleantoString = (b) => b.toString();
+
+    let bolean = true;
+    console.log(booleantoString(bolean));
+}
+
+if (algoritmo == "#68 operaciones basicas") {
+    function operacionesBasicas(operacion, n1, n2) {
+        switch (operacion) {
+            case "+":
+                return n1 + n2;
+                break;
+            case "-":
+                return n1 - n2;
+                break;
+            case "*":
+                return n1 * n2;
+                break;
+            case "/":
+                return n1 / n2;
+                break;
+            default:
+                return "ingreso una opcion incorrecta";
+                break;
+        }
+    }
+    console.log(operacionesBasicas("+", 5, 20));
+}
+
+if (algoritmo == "#69 numero a cadena") {
+    const numbertoString = (b) => b.toString();
+
+    let number = parseInt(80);
+    console.log(numbertoString(number));
+}
+
+if (algoritmo == "#70 el numero opuesto") {
+    const numeroOpuesto = (num) => -num;
+
+    alert(numeroOpuesto(prompt("ingrese un numero")));
+}
+
+if (algoritmo == "#71 obtener la raiz, cuadrada o cubica") {
+    function obtenerRaiz(raiz, num) {
+        if (raiz == "cuadrada") {
+            return Math.sqrt(num);
+        } else {
+            return Math.pow(num, 0.33);
+        }
+    }
+
+    console.log(`Raíz cuadrada = ${obtenerRaiz("cuadrada", 2)}`);
+    console.log(`Raíz cubica = ${obtenerRaiz("", 2)}`);
+}
+
+if (algoritmo == "#72 sumando numeros de un arreglo") {
+    const numeros = [2, 2, 4, 4];
+    const suma = numeros.reduce((x, y) => x + y);
+
+    alert(suma);
+}
+
+if (algoritmo == "#73 sumar los salarios") {
+    const salaries = {
+        Felix: 100,
+        Pete: 160,
+        Pedro: 130,
+    };
+    const sum = Object.values(salaries).reduce(
+        (total, salary) => total + salary,
+        0
+    );
+    console.log(sum);
+}
+
+if (algoritmo == "#74 multiplicar propiedades numericas por 2") {
+    const menu = {
+        width: 200,
+        height: 400,
+        title: "Mi menú",
+    };
+
+    multiplicNumeric(menu);
+}
+
+if (algoritmo == "#75 ¿Es un palindromo?") {
+    /* Escriba una función que determine
+    si una cadena dada es un palíndromo (es decir, se lee igual en ambos sentidos).
+    Por ejemplo, "racecar" es un palíndromo. */
+    function isPalindrome(str) {
+        str = str.toLowerCase();
+        return str === str.split("").reverse().join("");
+    }
+}
+
+if (algoritmo == "#76 Invertir una cadena") {
+    /* Escriba una función que acepte una
+    cadena y devuelva la cadena invertida.
+    Por ejemplo, "hello" debería devolver
+    "olleh". */
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+}
+
+if (algoritmo == "#77 Sumar elementos de una matriz") {
+    /* Escriba una función que acepte una
+    matriz de números y devuelva la suma de
+    todos los números en la matriz. */
+    function sumArray(numbers) {
+        return numbers.reduce((acc, current) => acc + current, 0);
+    }
+}
+
+if (algoritmo == "#78 Concatenar elementos de una matriz en una sola cadena") {
+    /* Escriba una función que acepte una
+    matriz de cadenas y devuelva una cadena
+    que consista en todas las cadenas de la
+    matriz concatenadas juntas. */
+    function concatStrings(strings) {
+        return strings.join("");
+    }
+}
+
+if (algoritmo == "#79 Contar subcadenas en una cadena") {
+    /* Escriba una función que acepte una
+    cadena y devuelva el número de veces que
+    aparece una subcadena específica en la cadena. */
+    function countSubstring(str, substr) {
+        return (str.match(new RegExp(substr, "g")) || []).length;
+    }
+}
+
+if (algoritmo == "#80 Convertir una cadena en una matriz de caracteres") {
+    /* Escriba una función que acepte una
+    cadena y devuelva una matriz de sus caracteres.
+    Por ejemplo, "hello" debería devolver
+    ['h', 'e', 'l', 'l', 'o']. */
+    function stringToArray(str) {
+        const arr = [];
+        for (let i = 0; i < str.length; i++) {
+            arr.push(str[i]);
+        }
+        return arr;
+    }
+}
+
+if (algoritmo == "#81 Filtrar números pares de una matriz") {
+    /* Escriba una función que acepte una
+    matriz de números y devuelva una nueva
+    matriz con solo los números pares. */
+    function getEvenNumbers(numbers) {
+        const evens = [];
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 === 0) {
+                evens.push(numbers[i]);
+            }
+        }
+        return evens;
+    }
+}
+
+if (algoritmo == "#82 Invertir el orden de las cadenas en una matriz") {
+    /* Escriba una función que acepte una
+    matriz de cadenas y devuelva una nueva
+    matriz con las cadenas en orden inverso. */
+    function reverseStrings(strings) {
+        const reversed = [];
+        for (let i = strings.length - 1; i >= 0; i--) {
+            reversed.push(strings[i]);
+        }
+        return reversed;
+    }
+}
+
+if (algoritmo == "#83 busqueda binaria") {
+    function binarySearch(array, target) {
+        let left = 0;
+        let right = array.length - 1;
+        while (left <= right) {
+            const middle = left + Math.floor((right - left) / 2);
+            if (array[middle] === target) {
+                return middle;
+            }
+            if (array[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+if (algoritmo == "#84 Quicksort") {
+    function quickSort(array) {
+        if (array.length <= 1) {
+            return array;
+        }
+        const pivot = array[array.length - 1];
+        const left = [];
+        const right = [];
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] < pivot) {
+                left.push(array[i]);
+            } else {
+                right.push(array[i]);
+            }
+        }
+        return [...quickSort(left), pivot, ...quickSort(right)];
+    }
+}
+
+if (algoritmo == "#85 Merge sort") {
+    function mergeSort(array) {
+        if (array.length <= 1) {
+            return array;
+        }
+        const middle = Math.floor(array.length / 2);
+        const left = array.slice(0, middle);
+        const right = array.slice(middle);
+        return merge(mergeSort(left), mergeSort(right));
+    }
+    function merge(left, right) {
+        const result = [];
+        while (left.length > 0 && right.length > 0) {
+            if (left[0] < right[0]) {
+                result.push(left.shift());
+            } else {
+                result.push(right.shift());
+            }
+        }
+        return [...result, ...left, ...right];
+    }
+}
+
+if (algoritmo == "#86 Eliminar el último elemento de una matriz") {
+    const arr = [1, 2, 3, 4, 5];
+    arr.pop(); // elimina el elemento 5 de la matriz
+    console.log(arr); // imprime [1, 2, 3, 4]
+}
+
+if (algoritmo == "#87 Calcular la suma de los elementos de una matriz") {
+    const arr = [1, 2, 3, 4, 5];
+    let sum = 0;
+    while (arr.length > 0) {
+        sum += arr.pop();
+    }
+    console.log(sum); // imprime 15
+}
+
+if (algoritmo == "#88 Invertir el orden de los elementos en una matriz") {
+    const arr = [1, 2, 3, 4, 5];
+    const reversedArr = [];
+    while (arr.length > 0) {
+        reversedArr.push(arr.pop());
+    }
+    console.log(reversedArr); // imprime [5, 4, 3, 2, 1]
+}
+
+if (
+    algoritmo ==
+    "#89 Extraer una submatriz de elementos de una matriz dada con el método slice()"
+) {
+    // Inicializamos una matriz de números enteros
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // Definimos una función que acepta dos índices (start y end) y extrae
+    // una submatriz de la matriz dada con los elementos comprendidos entre
+    // start y end (inclusive)
+    function extractSubarray(array, start, end) {
+        // Usamos el método slice() para extraer la submatriz desde la matriz dada
+        const subarray = array.slice(start, end + 1);
+        return subarray;
+    }
+
+    // Probar la función con diferentes valores de start y end
+    console.log(extractSubarray(arr, 0, 4)); // imprime [1, 2, 3, 4, 5]
+    console.log(extractSubarray(arr, 3, 7)); // imprime [4, 5, 6, 7, 8]
+    console.log(extractSubarray(arr, 5, 9)); // imprime [6, 7, 8, 9, 10]
+}
+
+if (algoritmo == "#90 Transformando la lista") {
+    //Tenemos una lista de números enteros y queremos eliminar todos los elementos
+    //pares y añadir el doble de cada elemento impar en su lugar.
+    let lista = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    let i = 0;
+    while (i < lista.length) {
+        if (lista[i] % 2 == 0) {
+            lista.splice(i, 1); // Eliminar el elemento par
+        } else {
+            lista.splice(i, 1, lista[i] * 2); // Reemplazar el elemento impar por su doble
+            i += 1; // Avanzar el índice en una posición
+        }
+    }
+
+    console.log(lista); // imprime [2, 6, 10, 14]
+}
