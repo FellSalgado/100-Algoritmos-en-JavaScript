ee0c58b9e47b46b0a1f5351f808c40f584f16e6b
Felix <salgadocanga@gmail.com>
2023-03-01 00:07:29 -0500

    se agrego los algoritmos #21, #22, ..., hasta el #50

--- algoritmos.js
+++ algoritmos.js
@@ -292,3 +292,350 @@ if (algoritmo == "#20 ¿numero par o impar?") {
         alert(`${numero} es impar`);
     }
 }
+
+if (algoritmo == "#21 años bisiestos") {
+    let anio = prompt("Ingrese un año");
+
+    if (anio % 400 == 0) {
+        alert(`${anio} es un año bixiesto`);
+    } else {
+        alert(`${anio} no es un año bixiesto`);
+    }
+}
+
+if (algoritmo == "#22 ¿Cual es mayor y cual es menor, o son iguales?") {
+    /* Desarrolle un algoritmo que le permita al usuario ingresar dos numeros y luego
+    le muestre cual de los dos numeros es el mayor, el menor o sí son iguales. */
+
+    let num1 = parseInt(prompt("Ingrese el primer numero"));
+    let num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+    if (num1 == num2) {
+        document.write("Los numeros son iguales");
+    } else if (num1 > num2) {
+        document.write(num1 + " Es mayor que " + n2);
+    } else {
+        document.write(num2 + " Es mayor que " + n1);
+    }
+}
+
+if (algoritmo == "#23 el perímetro de un rectangulo") {
+    let base = parseInt(prompt("Ingrese la base del rectangulo"));
+    let altura = parseInt(prompt("Ingrese la altura del rectangulo"));
+
+    let perimetro = (base + altura) * 2;
+    alert(`El perimetro del rectangulo es:\n ${perimetro}cm`);
+}
+
+if (algoritmo == "#24 el perímetro de un triangulo equilatero") {
+    let lados = parseInt(
+        prompt("Ingrese la longitud de los lados del triangulo")
+    );
+
+    let perimetro = lados * 3;
+    alert(`Por lo tanto el perimetro del triangulo es:\n ${perimetro}cm`);
+}
+
+if (algoritmo == "#25 el perímetro de un rombo") {
+    let lados = parseInt(prompt("Ingrese la longitud de los lados del rombo"));
+
+    let perimetro = lados * 4;
+    alert("Por lo tanto el perimetro del rombo es:\n" + perimetro);
+}
+
+if (algoritmo == "#26 el perímetro de un triangulo isósceles") {
+    let ladosRepetidos = parseInt(
+        prompt("Ingrese la longitud de los 2 lados que se repiten")
+    );
+    let ladoDiferente = parseInt(
+        prompt("Ingrese la longitud del lado diferente")
+    );
+
+    let perimetro = ladosRepetidos * 2 + ladoDiferente;
+    alert(
+        "Por lo tanto el perimetro del triangulo isósceles es:\n" + perimetro
+    );
+}
+
+if (algoritmo == "#27 el perímetro de un trapecio isósceles") {
+    let ladosOblicuos = 5;
+    let baseMayor = 12;
+    let baseMenor = 6;
+
+    let perimetro = ladosOblicuos * 2 + baseMayor + baseMenor;
+
+    alert("Por lo tanto el perimetro del trapecio isósceles es:\n" + perimetro);
+}
+
+if (algoritmo == "#28 el perímetro de un polígono escalonado") {
+    let base = 6;
+    let altura = 8;
+
+    let perimetro = 2 * (base + altura);
+
+    alert(`El perimetro del polígono escalonado es:\n ${perimetro}cm`);
+}
+
+if (algoritmo == "#29 litros a minilitros") {
+    let litros = parseInt(prompt("Ingrese la catidad de litros"));
+    let minilitros = litros * 1000;
+
+    alert(`${litros} litros equivalen a ${minilitros} minilitros`);
+}
+
+if (algoritmo == "#30 ¿necesito una a dieta?") {
+    let peso = parseInt(prompt("Ingrese su peso"));
+
+    alert(
+        peso < 200 ? "Usted está bien de peso" : "¡Usted se debe poner a dieta!"
+    );
+}
+
+if (algoritmo == "#31 centígrados a fahrenheit") {
+    let centigrados = parseFloat(prompt("Ingresa grados centígrados"));
+    let fahrenheit = (centigrados * 9) / 5 + 32;
+
+    console.log(
+        `${centigrados} centígrados equivalen a ${fahrenheit} fahrenheit`
+    );
+}
+
+if (algoritmo == "#32 fahrenheit a centígrados") {
+    let fahrenheit = parseFloat(prompt("Ingresa grados fahrenheit"));
+    let cetigrados = ((fahrenheit - 32) * 5) / 9;
+
+    console.log(
+        `${fahrenheit} fahrenheit equivalen a ${centigrados} centígrados`
+    );
+}
+
+if (algoritmo == "#33 el perímetro de cualquier polígono regular") {
+    let lado = parseInt(prompt("Ingrese la longitud de los lados"));
+    let cantidadDeLados = parseInt(
+        prompt("Ingrese la centidad de lados que tiene el polígono")
+    );
+
+    console.log(lado * cantidadDeLados);
+}
+
+if (algoritmo == "#34 litros a galones") {
+    /* elabore un algoritmo que permita convertir
+    un valor expresado en litros a su valor correspondiente en galones */
+
+    let litros = prompt("Ingrese la catidad de litros");
+    let galones = parseInt(litros / 3.785);
+
+    alert(`${litros} litros equivalen a ${galones} galones`);
+}
+
+if (algoritmo == "#35 el area de un circulo") {
+    let radio = parseFloat(prompt("Ingrese el radio del circulo"));
+    let area = Math.PI * (radio * 2);
+
+    alert(area < 100 ? `El area del circulo es de: ${area}cm²` : "error!");
+}
+
+if (algoritmo == "#36 mayor, menor o diferencia") {
+    const message = [];
+    let num1 = parseInt(prompt("Ingrese el primer numero"));
+    let num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+    function resultado(operacion, n1, n2) {
+        if (operacion > 3) {
+            return alert("Ingreso una opcion incorrecta");
+        } else {
+            if (n1 > n2) {
+                message[0] = n1 + " Es mayor";
+                message[1] = n2 + " Es menor";
+                message[2] = n1 - n2;
+            } else {
+                message[0] = n2 + " Es mayor";
+                message[1] = n1 + " Es menor";
+                message[2] = n2 - n1;
+            }
+            return alert(message[operacion - 1]);
+        }
+    }
+
+    let operacion = prompt(
+        "ingrese:\n[1] par encontrar el mayor\n[2] para encontrar el menor\n[3] para hayar la difererencia entre los dos numeros"
+    );
+    resultado(operacion, num1, num2);
+}
+
+if (algoritmo == "#37 elementos unicos") {
+    const frameworks = ["React", "Next", "Angular", "Vue", "Angular", "Next"];
+
+    const unicos = [...new Set(frameworks)];
+    ms(frameworks);
+    ms(unicos);
+}
+
+if (algoritmo == "#38 block de notas") {
+    let note = prompt("ingrese sus notas, separando una de otras con una coma");
+    let split = note.split("");
+    let from = Array.from(split);
+
+    console.log(split);
+    console.log(from);
+}
+
+if (algoritmo == "#39 carrito de compras") {
+    const productos = [];
+    let cantidad = parseInt(prompt("Digita cuantos productos desea guardar"));
+
+    for (let i = 1; i <= cantidad; i++) {
+        productos[i] = prompt(`producto ${i}`);
+    }
+
+    alert(`Sus productos fueron guardados exitosamente:\n ${productos}`);
+}
+
+if (algoritmo == "#40 sumando elementos de un arreglo") {
+    const numeros = [];
+    numeros[0] = parseInt(prompt("Ingrese el primer numero"));
+    numeros[1] = parseInt(prompt("Ingrese el segundo numero"));
+
+    let operacion = numeros[0] + numeros[1];
+
+    alert("El resultado es: " + operacion);
+}
+
+if (algoritmo == "#41 multiplicando elementos de un arreglo") {
+    const numeros = [];
+    numeros[0] = parseInt(prompt("Ingrese el primer numero"));
+    numeros[1] = parseInt(prompt("Ingrese el segundo numero"));
+
+    let operacion = numeros[0] * numeros[1];
+
+    alert(`El resultado es: ${operacion}`);
+}
+
+if (algoritmo == "#42 un formulario") {
+    function leer(value) {
+        return prompt("Ingrese " + value) + " ";
+    }
+
+    const datos = [];
+
+    datos[0] = leer("su nombre");
+    datos[1] = leer("su apellido");
+    datos[2] = leer("su edad");
+    datos[3] = leer("su tipo de documento");
+    datos[4] = leer("su numero de documento");
+
+    alert(`Sus datos ingresados fueron guardados exitosamente:\n ${datos}`);
+}
+
+if (algoritmo == "#43 buscar numeros pares") {
+    let cantidad = parseInt(prompt("Ingrese una cantidad de numeros"));
+    for (let i = 0; i < cantidad; i++) {
+        document.write(" " + i * 2);
+    }
+}
+
+if (algoritmo == "#44 juego de sumar") {
+    let i = true;
+    let num1 = 3;
+    let num2 = 2;
+    let puntos = 0;
+
+    while (i) {
+        let result = prompt(num1 + " + " + num2 + " =");
+
+        if (result != num1 + num2) {
+            alert("Ya perdiste🤦‍♂️");
+            i = false;
+        }
+        puntos++;
+        num1 += 2;
+        num2++;
+    }
+    alert("Tu puntuacion quedo en: " + puntos + " puntos");
+}
+
+if (algoritmo == "#45 piramide invertida") {
+    let pisos = prompt("Ingrese el numero de pisos");
+
+    document.write("<center>");
+    for (let i = pisos; i > 0; i--) {
+        for (let c = i; c > 0; c--) {
+            document.write(" " + c + " ");
+        }
+        document.write("<br>");
+    }
+    document.write("</center>");
+}
+
+if (algoritmo == "#46 mas el anterior") {
+    let num = 1;
+
+    while (num < 60) {
+        document.write(num);
+        num += num;
+    }
+}
+
+if (algoritmo == "#47 La sucesión de FIBONNACI") {
+    let first = 0;
+    let second = 1;
+
+    for (let i = 0; i < 20; i++) {
+        console.log(first);
+        const suma = first + second;
+
+        first = second;
+        second = suma;
+    }
+}
+
+if (algoritmo == "#48 de 1 en 1 hasta el 40") {
+    /*Desarrolle un programa que le permita al usuario ingresar un numero y este vaya
+    incrementandose de 1 en 1 hasta llegar al 40. Con la condición de que, sí digita un número mayor o igual a 40,
+    entonces este se irá desminuyendo de 1 en 1 hasta llegar a 0.*/
+
+    let numero = parseInt(prompt("Digite un numero"));
+    if (numero < 40) {
+        while (numero <= 40) {
+            console.log(numero);
+            console.log("<br>");
+            numero++;
+        }
+    } else {
+        while (numero >= 0) {
+            console.log(numero);
+            console.log("<br>");
+            numero--;
+        }
+    }
+}
+
+if (algoritmo == "#49 una alcancia") {
+    let dinero = 0;
+    let i = true;
+
+    while (i == true) {
+        let nuevoDinero = prompt(
+            "deposite la catidad nueva [de 0 en adelante, sin decimales] o digite 'ok' para terminar"
+        );
+
+        if ((nuevoDinero == "cancelar") | (nuevoDinero == "ok")) {
+            alert(`su dinero total depositado:\n $${dinero}`);
+            i = false;
+        } else if (nuevoDinero > 0) {
+            dinero += parseInt(nuevoDinero);
+            alert(dinero);
+        } else {
+            alert(
+                "por favor ingrese un valor valido o digite 'cancelar' para salir"
+            );
+        }
+    }
+}
+
+if (algoritmo == "#50 comprando dolares") {
+    let cartera = 10;
+    let dolares = cartera / 4.45;
+
+    alert(`con $${cartera} usted puede comprar ${dolares} USD`);
+}
